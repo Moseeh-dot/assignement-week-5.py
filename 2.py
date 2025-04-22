b74f479f9e2ba6b48b8ea7adff036e0b344e3522
Moseeh-dot <moses123.munene@gmail.com>
2025-04-22 14:39:40 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,24 @@
+class Animal:
+    def move(self):
+        print("The animal moves in its own way.")
+
+class Dog(Animal):
+    def move(self):
+        print("The dog runs 🐕.")
+
+class Bird(Animal):
+    def move(self):
+        print("The bird flies 🐦.")
+
+class Vehicle:
+    def move(self):
+        print("The vehicle moves in its own way.")
+
+class Car(Vehicle):
+    def move(self):
+        print("The car drives 🚗.")
+
+class Plane(Vehicle):
+    def move(self):
+        print("The plane flies ✈️.")
+
